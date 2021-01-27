5431a8dc788020461c7d9c609cb66affc2ff53a9
rzkyvr69 <77834048+rzkyvr69@users.noreply.github.com>
2021-01-27 18:49:33 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,15 @@
+<?php
+
+//BAHAN YANG PERLU DIINSTAL DI TERMUX
+//[1]. pkg install php
+//[2]. pkg install tesseract
+//[3]. pkg install imagemagick
+//Sampai Sini sudah Paham Kan 
+//Jangan Lupa Subscribe Chanel Penghasil Gratisa
+
+
+$ua = "Mozilla/5.0 (Linux; Android 10; RMX1851) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36";
+
+$PHPSESSID = "PHPSESSID=u8v0o5tg8colv2622a23j071nh";
+
+$get_id = "get_id=135839";
